880ecd743ddc0587fc377060352816ec8985b6c9
EBostonCoachUs <eric.boston@coachus.com>
2025-11-08 20:23:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import './index.css'
+
 
 export default function CoachUSLanding() {
   const [email, setEmail] = useState("");
